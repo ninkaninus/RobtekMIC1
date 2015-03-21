856946cb882d7bdeae4c8f226a129223aed28d65
ninkaninus <williamstrand8@gmail.com>
2015-03-21 19:14:30 +0100

    tab val = 4

--- micAflevering.asm
+++ micAflevering.asm
@@ -2,7 +2,7 @@
 ; |*************************************|
 ; | William Bergmann Børresen			|
 ; | AUR1 - Robtek						|
-; | Afleveres: dato 13-03-2015 kl 12:00	|
+; | Afleveres: dato 27-03-2015 kl 12:00	|
 ; |*************************************|
 ; | Written for MIC ATmega32A board		|
 ; | Afleverings opgave 2				|
@@ -22,119 +22,32 @@
 ;Initialisér programmet
 RESET:
 ; PORTC setup
-			LDI		R16, 0x00			;
-			OUT		DDRC, R16			; Set PORTC as input
-			LDI		R16, 255			;
-			OUT		PORTC,R16 			; Enable pull-up on PORTC
+							LDI		R16, 0x00				;
+							OUT		DDRC, R16				; Set PORTC as input
+							LDI		R16, 255				;
+							OUT		PORTC,R16 				; Enable pull-up on PORTC
 
 ; PORTB setup
-			OUT 	DDRB,R16 			; PORTB = output
-			LDI		R16, 0xFF
-			OUT		PORTB, R16			; Turn LEDS off
+							OUT 	DDRB,R16 				; PORTB = output
+							LDI		R16, 0xFF
+							OUT		PORTB, R16				; Turn LEDS off
 
 ;Start programløkken
 LOOP:
 
 Aseg:
-			LDI		R18, segA			;læs værdien af segA ind i R18
-			OUT		PORTB, R18			;skriv værdien af R18 til PORTB (dispalyet)
-			IN 		R17, PINC			;læs værdien af port c ind i R17
-			LDI		R20, 0x97			;læs værdien 0x97 ind 9 R20
-Loopa0:		LDI		R21, 0x06			;læs værdien 0x06 ind 9 R21
-Loopa1:		LDI		R22, 0x92			;læs værdien 0x92 ind 9 R22
-Loopa2:		DEC		R22					;træk en fra værdien i R22
-			BRNE	Loopa2				;hvis der kunne trækkes en fra R22 så gå til Loopa2
-			DEC		R21					;træk en fra værdien i R21
-			BRNE	Loopa1				;hvis der kunne trækkes en fra R21 så gå til Loopa1
-			DEC		R20					;træk en fra værdien i R20
-			BRNE	Loopa0				;hvis der kunne trækkes en fra R20 så gå til Loopa0
-			IN		R18, PINC			;læs værdien af port c ind i R18
-			CP		R17, R18			;Sammenlign R17 og R18
-			BREQ	Aseg				;Hvis CP gav true gå til Aseg
-
-Bseg:
-			LDI		R18, segB			;læs værdien af segB ind i R18
-			OUT		PORTB, R18			;skriv værdien af R18 til PORTB (dispalyet)
-			IN 		R17, PINC			;læs værdien af port c ind i R17
-			LDI		R20, 0x97			;læs værdien 0x97 ind 9 R20
-Loopb0:		LDI		R21, 0x06			;læs værdien 0x06 ind 9 R21
-Loopb1:		LDI		R22, 0x92			;læs værdien 0x92 ind 9 R22
-Loopb2:		DEC		R22					;træk en fra værdien i R22
-			BRNE	Loopb2				;hvis der kunne trækkes en fra R22 så gå til Loopb2
-			DEC		R21					;træk en fra værdien i R21
-			BRNE	Loopb1				;hvis der kunne trækkes en fra R21 så gå til Loopb1
-			DEC		R20					;træk en fra værdien i R20
-			BRNE	Loopb0				;hvis der kunne trækkes en fra R20 så gå til Loopb0
-			IN		R18, PINC			;læs værdien af port c ind i R18
-			CP		R17, R18			;Sammenlign R17 og R18
-			BREQ	Bseg				;Hvis CP gav true gå til Bseg
-
-Cseg:
-			LDI		R18, segC			;læs værdien af segC ind i R18
-			OUT		PORTB, R18			;skriv værdien af R18 til PORTB (dispalyet)
-			IN 		R17, PINC			;læs værdien af port c ind i R17
-			LDI		R20, 0x97			;læs værdien 0x97 ind 9 R20
-Loopc0:		LDI		R21, 0x06			;læs værdien 0x06 ind 9 R21
-Loopc1:		LDI		R22, 0x92			;læs værdien 0x92 ind 9 R22
-Loopc2:		DEC		R22					;træk en fra værdien i R22
-			BRNE	Loopc2				;hvis der kunne trækkes en fra R22 så gå til Loopc2
-			DEC		R21					;træk en fra værdien i R21
-			BRNE	Loopc1				;hvis der kunne trækkes en fra R21 så gå til Loopc1
-			DEC		R20					;træk en fra værdien i R20
-			BRNE	Loopc0				;hvis der kunne trækkes en fra R20 så gå til Loopc0
-			IN		R18, PINC			;læs værdien af port c ind i R18
-			CP		R17, R18			;Sammenlign R17 og R18
-			BREQ	Cseg				;Hvis CP gav true gå til Cseg
-
-Dseg:
-			LDI		R18, segD			;læs værdien af segD ind i R18
-			OUT		PORTB, R18			;skriv værdien af R18 til PORTB (dispalyet)
-			IN 		R17, PINC			;læs værdien af port c ind i R17
-			LDI		R20, 0x97			;læs værdien 0x97 ind 9 R20
-Loopd0:		LDI		R21, 0x06			;læs værdien 0x06 ind 9 R21
-Loopd1:		LDI		R22, 0x92			;læs værdien 0x92 ind 9 R22
-Loopd2:		DEC		R22					;træk en fra værdien i R22
-			BRNE	Loopd2				;hvis der kunne trækkes en fra R22 så gå til Loopd2
-			DEC		R21					;træk en fra værdien i R21
-			BRNE	Loopd1				;hvis der kunne trækkes en fra R21 så gå til Loopd1
-			DEC		R20					;træk en fra værdien i R20
-			BRNE	Loopd0				;hvis der kunne trækkes en fra R20 så gå til Loopd0
-			IN		R18, PINC			;læs værdien af port c ind i R18
-			CP		R17, R18			;Sammenlign R17 og R18
-			BREQ	Dseg				;Hvis CP gav true gå til Dseg
-
-Eseg:
-			LDI		R18, segE			;læs værdien af segE ind i R18
-			OUT		PORTB, R18			;skriv værdien af R18 til PORTB (dispalyet)
-			IN 		R17, PINC			;læs værdien af port c ind i R17
-			LDI		R20, 0x97			;læs værdien 0x97 ind 9 R20
-Loope0:		LDI		R21, 0x06			;læs værdien 0x06 ind 9 R21
-Loope1:		LDI		R22, 0x92			;læs værdien 0x92 ind 9 R22
-Loope2:		DEC		R22					;træk en fra værdien i R22
-			BRNE	Loope2				;hvis der kunne trækkes en fra R22 så gå til Loope2
-			DEC		R21					;træk en fra værdien i R21
-			BRNE	Loope1				;hvis der kunne trækkes en fra R21 så gå til Loope1
-			DEC		R20					;træk en fra værdien i R20
-			BRNE	Loope0				;hvis der kunne trækkes en fra R20 så gå til Loope0
-			IN		R18, PINC			;læs værdien af port c ind i R18
-			CP		R17, R18			;Sammenlign R17 og R18
-			BREQ	Eseg				;Hvis CP gav true gå til Eseg
-
-Fseg:
-			LDI		R18, segF			;læs værdien af segF ind i R18
-			OUT		PORTB, R18			;skriv værdien af R18 til PORTB (dispalyet)
-			IN 		R17, PINC			;læs værdien af port c ind i R17
-			LDI		R20, 0x97			;læs værdien 0x97 ind 9 R20
-Loopf0:		LDI		R21, 0x06			;læs værdien 0x06 ind 9 R21
-Loopf1:		LDI		R22, 0x92			;læs værdien 0x92 ind 9 R22
-Loopf2:		DEC		R22					;træk en fra værdien i R22
-			BRNE	Loopf2				;hvis der kunne trækkes en fra R22 så gå til Loopf2
-			DEC		R21					;træk en fra værdien i R21
-			BRNE	Loopf1				;hvis der kunne trækkes en fra R21 så gå til Loopf1
-			DEC		R20					;træk en fra værdien i R20
-			BRNE	Loopf0				;hvis der kunne trækkes en fra R20 så gå til Loopf0
-			IN		R18, PINC			;læs værdien af port c ind i R18
-			CP		R17, R18			;Sammenlign R17 og R18
-			BREQ	Fseg				;Hvis CP gav true gå til Fseg
-
-			rjmp		LOOP
+							LDI		R18, segA				;læs værdien af segA ind i R18
+							OUT		PORTB, R18				;skriv værdien af R18 til PORTB (dispalyet)
+							IN 		R17, PINC				;læs værdien af port c ind i R17
+							LDI		R20, 0x97				;læs værdien 0x97 ind 9 R20
+Loopa0:						LDI		R21, 0x06				;læs værdien 0x06 ind 9 R21
+Loopa1:						LDI		R22, 0x92				;læs værdien 0x92 ind 9 R22
+Loopa2:						DEC		R22						;træk en fra værdien i R22
+							BRNE	Loopa2					;hvis der kunne trækkes en fra R22 så gå til Loopa2
+							DEC		R21						;træk en fra værdien i R21
+							BRNE	Loopa1					;hvis der kunne trækkes en fra R21 så gå til Loopa1
+							DEC		R20						;træk en fra værdien i R20
+							BRNE	Loopa0					;hvis der kunne trækkes en fra R20 så gå til Loopa0
+							IN		R18, PINC				;læs værdien af port c ind i R18
+							CP		R17, R18				;Sammenlign R17 og R18
+							BREQ	Aseg					;Hvis CP gav true gå til Aseg

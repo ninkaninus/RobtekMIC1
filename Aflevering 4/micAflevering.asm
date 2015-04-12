c70d88fe6bd42a9db5e106fcad373e8feba15716
ninkaninus <williamstrand8@gmail.com>
2015-04-12 16:16:14 +0200

    description added

--- Aflevering 4/micAflevering.asm
+++ Aflevering 4/micAflevering.asm
@@ -1,18 +1,26 @@
 
-; |*************************************|
-; | William Bergmann Børresen - wiboe14	|
-; |	Daniel Franzéen Haraldson - dahar14	|
-; |	Joakim Grøn - 						|
-; | AUR1 - Robtek						|
-; | Afleveres: dato 					|
-; |*************************************|
-; | Written for MIC ATmega32A board		|
-; | Afleverings opgave 4				|
-; |*************************************|
+; |*****************************************|
+; |											|
+; | AUR1 - Robtek							|
+; |											|
+; | William Bergmann Børresen 		wiboe14	|
+; |	Daniel Franzéen Haraldson 		dahar14	|
+; |	Joakim Grøn 					jokor11	|
+; |											|
+; |*****************************************|
+; |											|
+; | Afleveres: dato: 17/04/2015				|
+; |											|
+; |*****************************************|
+; |											|
+; | Written for MIC ATmega32A board			|
+; | Afleverings opgave 4					|
+; |											|
+; |*****************************************|
 
 .include "m32def.inc"
 
-;Initialisér programmet
+;Initialize programmet
 
 ;Entry point
 .org	0x0000
@@ -20,12 +28,12 @@ RJMP	INIT
 
 INIT:
 ; PORTB setup
-						SBI 	DDRB, 3				;PORTB(3)
+						SBI 	DDRB, 3					;PORTB(3)
 
 
 ; Timer0 setup
 						LDI		R16, 0b01110001
-						OUT		TCCR0, R16
+						OUT		TCCR0, R16				;Timer0 setup
 
 ; PORTA setup
 						LDI		R16, 0
@@ -35,22 +43,22 @@ INIT:
 						LDI		R16, 0b11100011
 						OUT     ADCSRA, R16				;Set prescaler
 
-						RJMP	LOOP
+						RJMP	LOOP					;Jump to LOOP
 
 LOOP:
-;Start programløkken
+;Start the loop
 
-						SBI		ADCSRA, ADSC				;start new coversion
+						SBI		ADCSRA, ADSC			;start new coversion
 
 WAIT:
-;wait for adc to be ready
-						SBIS	ADCSRA, ADIF				;wait for ADC
-						RJMP	WAIT
-						SBI		ADCSRA, ADIF
+;wait for ADC to be ready
+						SBIS	ADCSRA, ADIF			;Check if the ADC has set the flag
+						RJMP	WAIT					;If not jump  to WAIT
+						SBI		ADCSRA, ADIF			;Clear ADC flag
 
 ;continue loop
-						IN		R16, ADCL
-						IN		R16, ADCH
-						OUT		OCR0, R16
+						IN		R16, ADCL				;Read ADC low byte into R16
+						IN		R16, ADCH				;Read ADC high byte into R16 to overrite low byte
+						OUT		OCR0, R16				;Output the value from R16 (ADC HIGH) into the value for the timer to compare with
 
-						RJMP	LOOP
+						RJMP	LOOP					;Jump to LOOP

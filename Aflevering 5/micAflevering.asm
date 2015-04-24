8ea2beb3a3f9fcbf47fdefc5ccc1a3cc3fb5ab88
ninkaninus <williamstrand8@gmail.com>
2015-04-24 10:19:59 +0200

    aflevering 6 started

--- Aflevering 5/micAflevering.asm
+++ Aflevering 5/micAflevering.asm
@@ -43,14 +43,14 @@ MAIN:
 
 
 ;Setup Timer0 til CTC mode, 10 ticks
-	;LDI 	R16, 	0b00001101
-	;OUT 	OCR0, 	R16
+	LDI 	R16, 	0b00001001
+	OUT 	OCR0, 	R16
 	LDI 	R16, 	0x09 ;Ingen prescaler
 	OUT 	TCCR0, 	R16
 	LDI 	R20, 	0
  	OUT 	TCNT0, 	R20
-	LDI 	R20, 	25
- 	OUT		OCR0,	R20
+	;LDI 	R20, 	25
+ 	;OUT		OCR0,	R20
 
 
 ;Opsæt PORTB til output
@@ -60,10 +60,13 @@ MAIN:
 	OUT 	PORTB,	R16
 
 ; Initialize ... Whatever
-	LDI 	R16, 	0b00000101 	;1-tal i LSB
+	LDI 	R16, 	50 	;1-tal i LSB
 
 LOOP:
 	LDI		R16,	0x00
+	OUT 	PORTB,	R16
+	Call	DELAY_1MS
+	LDI		R16,	0xFF
 	OUT 	PORTB,	R16
 	JMP 	LOOP
 
@@ -72,14 +75,16 @@ OFC0_ISR:
 	LDI		R16,	0x00
 	OUT 	PORTB,	R16
 	Call	DELAY_1MS
-	RETI 	;Afslut ISR
+	LDI		R16,	0xFF
+	OUT 	PORTB,	R16
+	JMP 	LOOP
 
 
 DELAY_1MS:
 		DELAY_1MS_C:										;									------------|
-							LDI		R17, 125				;load 125 ind i R17			(1 Cycle)-------|	|
+							LDI		R17, 120				;load 125 ind i R17			(1 Cycle)-------|	|
 			DELAY_1MS_B:									;											|	|
-							LDI		R18,20					;load 1 ind i R18			(1 Cycle)---|	|	|
+							LDI		R18,10					;load 1 ind i R18			(1 Cycle)---|	|	|
 				DELAY_1MS_A:								;										|	|	|
 							NOP								;Do nothing for 1 cycle		(1 Cycle)	|A	|B	|C
 							DEC		R18						;R18 - 1					(1 Cycle)	|	|	|

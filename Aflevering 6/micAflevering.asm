c390c071feaa27ae9071b27d502b169e9de3b264
William Bergmann Børresen <ninkaninus@users.noreply.github.com>
2015-04-24 21:33:39 +0200

    I have written comments to all the code
    new comments and changed the danish to english

--- Aflevering 6/micAflevering.asm
+++ Aflevering 6/micAflevering.asm
@@ -21,15 +21,15 @@
 .include "m32def.inc"
 
 SETUP:
-;Slå seriel kommunikation til
+;Turn on Serial comunnication
 	LDI		R16,	(1<<RXEN)|(1<<TXEN)
 	OUT		UCSRB,	R16
 
-;Brug UCSRC, sæt frame size = 8
+;Use UCSRC, set frame size = 8
 	LDI		R16,	(1<<URSEL)|(1<<UCSZ1)|(1<<UCSZ0)
 	OUT		UCSRC,	R16
 
-;Sæt baud rate = 1200 (UBRR = 41510 = 0x19F)
+;Set baud rate = 1200 (UBRR = 41510 = 0x19F)
 
 	LDI		R16,	(1<<U2X)		;U2X = 1
 	OUT		UCSRA,	R16
@@ -38,32 +38,32 @@ SETUP:
 
 LOOP:
 
-MODTAG:
-	SBIS	UCSRA,	RXC				;Hvis RXC=1, så er UDR klar med ny inddata
-	RJMP	MODTAG
-	IN		R17,	UDR				;Modtag data
-	mov 	R18,	R17
-	subi	R18,	'A'
-	brmi	SEND
-	subi	R18,	26
-	brmi	UPPER_TO_LOWER
-	subi	R18,	6
-	brmi	SEND
-	subi	R18,	26
-	brmi	LOWER_TO_UPPER
-	RJMP	SEND
+RECIVE:
+	SBIS	UCSRA,	RXC				;If RXC=1, Then UDR is ready to recive new data
+	RJMP	RECIVE					;Jump to MODATG if no data recived
+	IN		R17,	UDR				;Recive data
+	mov 	R18,	R17				;Move the recived value into R18 from R17
+	subi	R18,	'A'				;Substract the ASCII number for A (65) from the recived number
+	brmi	SEND					;If the value is below 0 jump to SEND
+	subi	R18,	26				;Substract 26 (number of caracteres in the english alphabet) from R18
+	brmi	UPPER_TO_LOWER				;if below 0 jump to UPPER_TO_LOWER
+	subi	R18,	6				;Substract 6 (the number of non letters between upper case and lower case letters) from R18 
+	brmi	SEND					;If below 0 jump to LOWER_TO_UPPER
+	subi	R18,	26				;Substract 26 (number of caracteres in the english alphabet) from R18
+	brmi	LOWER_TO_UPPER				;If below 0 jump to LOWER_TO_UPPER
+	RJMP	SEND					;If non of the other cases was true then jump to SEND
 
 UPPER_TO_LOWER:
-	LDI		R18,	32
-	ADD		R17,	R18
-	RJMP	SEND
+	LDI		R18,	32			;Load the value 32 (the difference between upper an lover case in ASCII) into R18
+	ADD		R17,	R18			;Add R18 (32) to R17
+	RJMP	SEND					;Jump to SEND
 
 LOWER_TO_UPPER:
-	SUBI	R17,	32
-	RJMP	SEND
+	SUBI	R17,	32				;Substract 32 (the difference between upper an lover case in ASCII) from R17
+	RJMP	SEND					;Jump to SEND
 
 SEND:
-	SBIS	UCSRA,	UDRE			;Hvis UDRE=1, så er UDR tom og klar til ny uddata
-	RJMP	SEND
-	OUT		UDR,	R17				;Send data
-	RJMP	LOOP					;Fortsæt uendeligt
+	SBIS	UCSRA,	UDRE			;IF UDRE=1, UDR is empty and ready to new data
+	RJMP	SEND					;Jump to SEND if the data line is not bussy
+	OUT		UDR,	R17				;SEND data
+	RJMP	LOOP					;Loop forever
